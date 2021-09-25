4403f7067393a659161ab00ad9f12a8d7c67d1f5
michasiok <michasio25@gmail.com>
2021-09-25 12:18:46 +0200

    cw.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
+        int liczba = 1000;
+        int nrIndexu = 57680;
+        do{
+            liczba--;
+            if(liczba % 80 == 0){
+                System.out.println("Liczba " + liczba + " jest podzielna przez 80");
+            }
+        }while(liczba>0);
+
+        int[] tablicaInt = {23,52,143,1,-100};
+        int iteratorInt = 0;
+        while (iteratorInt<tablicaInt.length){
+            System.out.println(tablicaInt[iteratorInt]);
+            iteratorInt++;
+        }
+
+        double[] tablicaDouble = {23.01,52.123,143.2,1.0,-100.78};
+        int iteratorDouble = 0;
+        while (iteratorDouble<tablicaDouble.length){
+            System.out.println(tablicaDouble[iteratorDouble]);
+            iteratorDouble++;
+        }
+        String[] tablicaString = {"qwe", "rty", "uio", "asd", "ghj"};
+        int iteratorString = tablicaString.length-1;
+        while (iteratorString >= 0){
+            System.out.println(tablicaString[iteratorString]);
+            iteratorString--;
+        }
+//
+//        while(true){
+//          nieskończona pętla
+//        }
+
+//        while(false){
+//            nigdy sie nie wykona
+//        }
+//
 
     }
 }
