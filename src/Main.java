64738a5b3c519d78127b8c351268d1ba20818eda
michasiok <michasio25@gmail.com>
2021-09-25 13:49:50 +0200

    cw.4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Petla break");
+        for (int i = 0; i < 10; i++) {
+            if(i == 3){
+                System.out.println("break");
+                break;
+            }
+            System.out.println("i="+i);
+        }
+        System.out.println();
+        System.out.println("Petla continue");
+        for (int j = 0; j < 10; j++) {
+            if (j==3){
+                System.out.println("continue");
+                continue;
+            }
+            System.out.println("j="+j);
+        }
 
+        System.out.println();
+        System.out.println("Petla return");
+        for (int k = 0; k < 10; k++) {
+            if (k==3){
+                System.out.println("return");
+                return;
+            }
+            System.out.println("k="+k);
+        }
     }
 }
